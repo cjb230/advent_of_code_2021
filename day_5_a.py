24caa37842b021bc1515d0269642424776972086
cjb230 <james.barton@blueyonder.co.uk>
2021-12-05 19:53:39 +0100

    Day 5

---
+++ day_5_a.py
@@ -0,0 +1,68 @@
+"""
+The obvious memory improvement for this one is to only write non-zero numbers of vents, and avoid creating a million-
+element dict of dicts. But it's fast enough anyway!
+"""
+
+SOURCE_FILE = 'day_5_input.txt'
+
+
+def pos_string_to_nums(position_string: str) -> dict:
+    number_strings = position_string.split(',')
+    x_pos = int(number_strings[0])
+    y_pos = int(number_strings[1])
+    return {'x': x_pos, 'y': y_pos}
+
+
+def points_from_line(line_def: list[dict]) -> list:
+    is_vertical = False
+    is_horizontal = False
+    points = []
+    #print(line_def)
+    ord_min = -1
+    ord_max = -1
+    if line_def[0]['x'] == line_def[1]['x']:
+        is_vertical = True
+        ord_min = min(line_def[0]['y'], line_def[1]['y'])
+        ord_max = max(line_def[0]['y'], line_def[1]['y'])
+        points = [{'x': line_def[0]['x'], 'y': i} for i in range(ord_min, ord_max+1)]
+    else:
+        is_horizontal = True
+        ord_min = min(line_def[0]['x'], line_def[1]['x'])
+        ord_max = max(line_def[0]['x'], line_def[1]['x'])
+        points = [{'x': i, 'y': line_def[0]['y']} for i in range(ord_min, ord_max+1)]
+    #print(points)
+    return points
+
+
+def main():
+    lines = []
+    vent_map = {x: {y: 0 for y in range(1000)} for x in range(1000)}
+
+    # read the file
+    with open(SOURCE_FILE) as f:
+        line_strings = f.read().splitlines()
+
+    # parse the file to a list of non-diagonal lines
+    for line_string in line_strings:
+        chunks = line_string.split(' ')
+        pos_1 = pos_string_to_nums(chunks[0])
+        pos_2 = pos_string_to_nums(chunks[2])
+        if (pos_1['x'] == pos_2['x']) or (pos_1['y'] == pos_2['y']):
+            lines.append([pos_1, pos_2])
+
+    # for each line, mark the vent locations on the vent map
+    for line in lines:
+        for point in points_from_line(line):
+            vent_map[point['x']][point['y']] += 1
+
+    total_intersections = 0
+    for x in vent_map:
+        for y, val in vent_map[x].items():
+            if val > 1:
+                total_intersections += 1
+    print(total_intersections)
+
+
+if __name__ == "__main__":
+    main()
+
